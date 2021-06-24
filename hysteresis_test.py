ae3e585a64a250fbbbe3bf200261302539ba0427
LukasRier <45269525+LukasRier@users.noreply.github.com>
2021-06-24 22:44:34 +0100

    add hysteresis test

---
+++ hysteresis_test.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 21:17:56 2021
+
+@author: lukas
+"""
+import matplotlib.pyplot as plt
+import pandas as pd
+from tkinter import filedialog
+import tkinter as tk
+import os
+
+import re
+
+root = tk.Tk()
+root.withdraw()
+
+file = filedialog.askopenfilename(parent=root,
+                                              filetypes=[('Text files',
+                                                          '*.csv')])
+filepath = os.path.abspath(file)
+pattern = re.compile(r'Cycle_\d+')
+fname_match = re.findall(pattern,file)
+
+if len(fname_match) != 1:
+    raise ValueError('This file is not named correctly.\n Generate separate cycling files using the GUI')
+
+cycle_no = re.findall(r'Cycle_(\d+).csv',file)[0]
+df = pd.read_csv(filepath)
+
+time_head = "Elapsed time/s "
+potential_head = "Ecell/V "
+capacity_head = "Capacity/mA.h.g^-1 "
+
+c_pot_name = potential_head + "(C" + cycle_no + ")"
+c_cap_name = capacity_head + "(C" + cycle_no + ")"
+
+c_potential = df.loc[:,c_pot_name]
+c_capacity = df.loc[:,c_cap_name]
+
+d_pot_name = potential_head + "(D" + cycle_no + ")"
+d_cap_name = capacity_head + "(D" + cycle_no + ")"
+
+d_potential = df.loc[:,d_pot_name]
+d_capacity = df.loc[:,d_cap_name]
+
+d_capacity = -1*d_capacity + d_capacity[d_capacity.index.get_loc(d_capacity.last_valid_index())]
+
+
+plt.figure()
+plt.plot(c_capacity,c_potential,'b')
+plt.plot(d_capacity,d_potential,'r')
+plt.xlabel("Capacity $\mathrm{mAh g^{-1}}$")
+plt.ylabel("Potential V")
