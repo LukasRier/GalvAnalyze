c60ea69e57bab58ea6924bf95ee4adbc89df0efc
LukasRier <45269525+LukasRier@users.noreply.github.com>
2021-07-04 23:06:44 +0100

    hysteresis scratchpad, now broken

--- hysteresis_test.py
+++ hysteresis_test.py
@@ -2,7 +2,7 @@
 """
 Created on Thu Jun 24 21:17:56 2021
 
-@author: lukas
+@author: Lukas Rier
 """
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -17,8 +17,8 @@ root.withdraw()
 
 ## hysteresis callback start
 file = filedialog.askopenfilename(parent=root,
-                                              filetypes=[('Text files',
-                                                          '*.csv')])
+                                  filetypes=[('Text files','*.csv')],
+                                  title='Open CSV file for a specific cycle')
 filepath = os.path.abspath(file)
 pattern = re.compile(r'Cycle_\d+')
 fname_match = re.findall(pattern,file)
@@ -28,22 +28,23 @@ if len(fname_match) != 1:
 
 cycle_no = re.findall(r'Cycle_(\d+).csv',file)[0]
 df = pd.read_csv(filepath)
-
-time_head = "Elapsed time/s "
-potential_head = "Ecell/V "
-capacity_head = "Capacity/mA.h.g^-1 "
-
-c_pot_name = potential_head + "(C" + cycle_no + ")"
-c_cap_name = capacity_head + "(C" + cycle_no + ")"
-
-c_potential = df.loc[:,c_pot_name]
-c_capacity = df.loc[:,c_cap_name]
-
-d_pot_name = potential_head + "(D" + cycle_no + ")"
-d_cap_name = capacity_head + "(D" + cycle_no + ")"
-
-d_potential = df.loc[:,d_pot_name]
-d_capacity = df.loc[:,d_cap_name]
+def hysteresis_data_from_frame(df):
+    # time_head = "Elapsed time/s "
+    potential_head = "Ecell/V "
+    capacity_head = "Capacity/mA.h.g^-1 "
+    
+    c_pot_name = potential_head + "(C" + cycle_no + ")"
+    c_cap_name = capacity_head + "(C" + cycle_no + ")"
+    
+    c_potential = df.loc[:,c_pot_name]
+    c_capacity = df.loc[:,c_cap_name]
+    
+    d_pot_name = potential_head + "(D" + cycle_no + ")"
+    d_cap_name = capacity_head + "(D" + cycle_no + ")"
+    
+    d_potential = df.loc[:,d_pot_name]
+    d_capacity = df.loc[:,d_cap_name]
+    return c_capacity,c_potential,d_capacity,d_potential
 ## end of callback, run plot hysteresis from within
 
 # plot hysteresis function receives c/d_potential c/d_capacity, flips d_cap and plots
